36d1dcfab564dcac06347f77f53718cafa7b54df
陈双庆 <thetopuser@gmail.com>
2026-09-14 19:06:00 +0800

    fix: remove deprecated verified parameter

--- Casks/auracalc.rb
+++ Casks/auracalc.rb
@@ -2,8 +2,7 @@ cask "auracalc" do
   version "1.0.0"
   sha256 "307bb1f2aa037ebd5e4ef337970fcdb9beb841c96aa9e09fd12e7ccc75e7567c"
 
-  url "https://icret.github.io/AuraCalc-Free/AuraCalc-latest.dmg",
-      verified: "icret.github.io/AuraCalc-Free"
+  url "https://icret.github.io/AuraCalc-Free/AuraCalc-latest.dmg"
   name "AuraCalc"
   desc "Lightweight macOS calculator written in pure Swift"
   homepage "https://icret.github.io/AuraCalc-Free/"
